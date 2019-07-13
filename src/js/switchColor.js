1a3ff5a2a1a357aa8248da6c04f401ae911c0427
Anatolii Petryshyn <a.petryshyn777@gmail.com>
2019-07-14 00:29:41 +0300

    added styles

--- src/js/switchColor.js
+++ src/js/switchColor.js
@@ -11,6 +11,7 @@ const refs = {
   body: document.querySelector('body'),
   startBtn: document.querySelector('button[data-action="start"]'),
   stopBtn: document.querySelector('button[data-action="stop"]'),
+  showColorCode: document.querySelector('.js-color'),
 };
 
 const randomIntegerFromInterval = (min, max) => {
@@ -26,15 +27,18 @@ let intervalId;
 function handleStartBtnClick(e) {
   intervalId = setInterval(switchColor, 1000, colors);
   refs.startBtn.disabled = true;
+  refs.startBtn.classList.add('disabledBtn');
 }
 
 function switchColor(colors) {
-  refs.body.style.backgroundColor =
-    colors[randomIntegerFromInterval(0, colors.length - 1)];
-  console.log('intrval');
+  let currentColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
+  refs.body.style.backgroundColor = currentColor;
+
+  refs.showColorCode.innerHTML = currentColor;
 }
 
 function handleStopBtnClick(e) {
   clearInterval(intervalId);
   refs.startBtn.disabled = false;
+  refs.startBtn.classList.remove('disabledBtn');
 }
